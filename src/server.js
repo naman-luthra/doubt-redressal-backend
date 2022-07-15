83a613a2590bad23020c4baaaddbeb9150023b32
Naman Luthra <namanluthra@Namans-MacBook-Air.local>
2022-07-16 00:45:19 +0530

    Fix CORS issue

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://doubt-redresser.netlify.app'
 }));
 
 Routes.forEach(Route=>app[Route.method](Route.path, Route.handler));
